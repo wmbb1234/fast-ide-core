c21aaa913b4bdc822cf22780883925844e2ed8d0
wmbb1234 <18131787494@163.com>
2026-06-15 14:30:43 +0800

    a

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-fn main() {
-    println!("Hello, world!");
-    println!("nobit");
+use eframe::egui;
+fn main() -> eframe::Result<()> {
+    let options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "windows",
+        options,
+        Box::new(|_cc| Ok(Box::new(MyApp))),
+    )
+}
+
+struct MyApp;
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("guhberaukg i");
+            if ui.button("click me").clicked() {
+                println!("zz");
+            }
+        });
+    }
 }
